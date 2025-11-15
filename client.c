3249b2c92ce534b730a1c6a4e6a186b5f73ca1ea
German Zittermann <gemanzittermann@gmail.com>
2025-11-15 17:25:14 -0300

    IP Error fixed

--- client.c
+++ client.c
@@ -26,6 +26,13 @@ int main(int argc, char *argv[]) {
     
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(8080);
+    
+    // Convert IP address from string to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        close(sockfd);
+        exit(1);
+    }
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
